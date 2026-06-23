d310044e79e97c31cb9fbda0c17cb0771998da7f
CutebatDev <alexaverten@gmail.com>
2026-06-23 16:23:16 +0300

    Base button

---
+++ Assets/Scripts/CharacterButtonVisuals.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterButtonVisuals : MonoBehaviour
+{
+    [SerializeField] private GameObject blockingPanel;
+    [SerializeField] private Button CharacterButton;
+    public bool IsTaken { get; private set; } = false; 
+
+    private void Start()
+    {
+        blockingPanel.SetActive(false);
+    }
+
+    public void ToggleTaken()
+    {
+        IsTaken = !IsTaken;
+        blockingPanel.SetActive(IsTaken);
+        CharacterButton.interactable = !IsTaken;
+    }
+}
