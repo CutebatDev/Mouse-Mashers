dbf3f6ee2360a60ea92d1118bdab579e4bc29ea2
CutebatDev <alexaverten@gmail.com>
2026-06-26 13:49:39 +0300

    i think this works

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -8,6 +8,7 @@ public class EnemyController : NetworkBehaviour
     [SerializeField] private DamageNumber damagePrefab;
     [SerializeField] private float maxHealth;
     [SerializeField] private float iFrameDuration = 0.25f;
+    [SerializeField] private EnemyAI enemyAI;
     [SerializeField] private SpriteRenderer sr;
 
     private bool isTakingDamage = false;
@@ -16,27 +17,14 @@ public class EnemyController : NetworkBehaviour
     [Networked] private float currentHealth { get; set; }
 
     private float lastDamageTime;
-
     private RectTransform worldCanvas;
 
-    [CanBeNull] private NetworkRunner networkRunner;
-    
-    
     public override void Spawned()
     {
         base.Spawned();
-        currentHealth = maxHealth;    }
-
-    private NetworkRunner GetNetworkRunner()
-    {
-        if (!networkRunner)
-        {
-            networkRunner = GameManager.Instance.networkRunner;
-        }
-        return networkRunner;
-        
+        currentHealth = maxHealth;
     }
-    
+
     public void SetWorldCanvasRef(RectTransform canvas)
     {
         worldCanvas = canvas;
@@ -45,11 +33,10 @@ public class EnemyController : NetworkBehaviour
     private void ShowDamage(float amount, Vector3 position)
     {
         DamageNumber number = Instantiate(damagePrefab, position, Quaternion.identity, worldCanvas);
-        number.transform.SetParent(worldCanvas, false);
         number.transform.SetPositionAndRotation(position, Quaternion.identity);
         number.SetDamage(amount);
     }
-
+    
     [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
     public void RPC_TakeDamage(float amount)
     {
@@ -57,54 +44,72 @@ public class EnemyController : NetworkBehaviour
             return;
 
         lastDamageTime = Time.time;
-        
+
         currentHealth -= amount;
-        
-        if (currentHealth <= 0)
+
+        if (currentHealth <= 0 && !isDead)
         {
             currentHealth = 0;
             isDead = true;
+
+            StartDeathSequence();
         }
     }
 
     public void TakeDamage(float amount)
     {
-        if (amount > 0)
-        {
-            ShowDamage(amount, transform.position);
-            TakeDamageAnimation();
-        }
-        
-        RPC_TakeDamage(amount);
-        
-    }
+        if (amount <= 10)
+            return;
+        ShowDamage(amount, transform.position);
+        TakeDamageAnimation();
 
+        RPC_TakeDamage(amount);
+    }
+    
     private void TakeDamageAnimation()
     {
-        if (isTakingDamage)
+        if (isTakingDamage || isDead)
             return;
 
         isTakingDamage = true;
 
-        var originalColor = sr.color;
-        
+        Color originalColor = sr.color;
+
         sr.DOKill();
-        
         sr.color = Color.red;
-        
+
         DOVirtual.DelayedCall(iFrameDuration, () =>
         {
+            if (sr == null) return;
+
             sr.color = originalColor;
             isTakingDamage = false;
-            if(isDead)
-                Die();
-        });
+        }).SetLink(gameObject);
     }
-    
+
+
+    private void StartDeathSequence()
+    {
+        enemyAI.StopMovement();
+        sr.DOKill();
+
+        RPC_PlayDeathVFX();
+
+        DOVirtual.DelayedCall(iFrameDuration, () =>
+        {
+            Die();
+        }).SetLink(gameObject);
+    }
+
+    [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
+    private void RPC_PlayDeathVFX()
+    {
+        sr.color = Color.red;
+    }
+
     private void Die()
     {
         EnemyRegistry.Instance.UnRegister(this);
         GameManager.Instance.networkRunner.Despawn(Object);
     }
 }
-
