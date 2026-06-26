bb370a3636791aa33ac83062a2ab7aab7fce162a
Tamir Yamaleyev <tamirka8@gmail.com>
2026-06-26 13:50:46 +0300

    Finished simple chat

---
+++ Assets/Scripts/UI/Chat/ChatNetworkManager.cs
@@ -0,0 +1,130 @@
+using Fusion;
+using Fusion.Sockets;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class ChatNetworkManager : MonoBehaviour, INetworkRunnerCallbacks
+{
+    [Header("References")]
+    [SerializeField] private NetworkRunner runnerPrefab;
+    [SerializeField] private NetworkObject multiplayerChatPrefab;
+
+    private NetworkRunner chatRunner;
+
+    async void Start()
+    {
+        await CreateChatRunner();
+    }
+
+    private async Task CreateChatRunner()
+    {
+        chatRunner = Instantiate(runnerPrefab);
+        chatRunner.AddCallbacks(this);
+
+        StartGameResult result = await chatRunner.StartGame(new StartGameArgs
+        {
+            GameMode = GameMode.Shared,
+            SessionName = "GlobalChat",
+            PlayerCount = 100
+        });
+
+        if (!result.Ok)
+        {
+            Debug.LogError($"Chat failed: {result.ErrorMessage}");
+            return;
+        }
+
+        if (chatRunner.IsSharedModeMasterClient)
+        {
+            chatRunner.Spawn(multiplayerChatPrefab);
+        }
+
+        Debug.Log("Chat connected");
+    }
+
+
+
+
+    // USELESS CALLBACKS BELOW!!!
+
+
+
+
+    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
+    {
+    }
+
+    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
+    {
+    }
+
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    {
+    }
+
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
+    {
+    }
+
+    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
+    {
+    }
+
+    public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason)
+    {
+    }
+
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
+    {
+    }
+
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
+    {
+    }
+
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
+    {
+    }
+
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data)
+    {
+    }
+
+    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress)
+    {
+    }
+
+    public void OnInput(NetworkRunner runner, NetworkInput input)
+    {
+    }
+
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
+    {
+    }
+
+    public void OnConnectedToServer(NetworkRunner runner)
+    {
+    }
+
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
+    {
+    }
+
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
+    {
+    }
+
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
+    {
+    }
+
+    public void OnSceneLoadDone(NetworkRunner runner)
+    {
+    }
+
+    public void OnSceneLoadStart(NetworkRunner runner)
+    {
+    }
+}
